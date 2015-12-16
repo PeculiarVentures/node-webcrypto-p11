3ddaad2870275548db78ce2b064aaa754b369cfc
Miroshin Stepan <microshine@mail.ru>
2015-12-16 21:38:42 +0300

    Fix sign, verify in subtle

--- lib/subtlecrypto.ts
+++ lib/subtlecrypto.ts
@@ -32,13 +32,13 @@ export class P11SubtleCrypto implements iwc.ISubtleCrypto {
 					algClass = rsa.RsaPKCS1
 					break;
 				case rsa.RsaPSS.ALGORITHM_NAME.toLowerCase():
-					algClass = rsa.RsaOAEP
+					algClass = rsa.RsaPSS
 					break;
 				case rsa.RsaOAEP.ALGORITHM_NAME.toLowerCase():
 					algClass = rsa.RsaOAEP
 					break;
 				default:
-					throw new TypeError("");
+					throw new TypeError("Unsupported algorith in use");
 			}
 			var key = algClass.generateKey(that.session, _alg, extractable, keyUsages);
 			resolve(key);
@@ -46,10 +46,54 @@ export class P11SubtleCrypto implements iwc.ISubtleCrypto {
 	}
 
 	sign(algorithm: iwc.AlgorithmType, key: CryptoKey, data: Buffer): Promise {
+		var that = this;
 		return new Promise(function(resolve, reject) {
-			//TODO: check algorithm
-			var signature: Buffer = key.key.sign(data);
+			var _alg: iwc.IAlgorithmIdentifier = { name: "" };
+			if (algorithm instanceof String) {
+				_alg = { name: algorithm };
+			}
+			else {
+				_alg = <iwc.IAlgorithmIdentifier>algorithm;
+			}
+			var algClass: alg.IAlgorithmBase = null;
+			switch (_alg.name.toLowerCase()) {
+				case rsa.RsaPKCS1.ALGORITHM_NAME.toLowerCase():
+					algClass = rsa.RsaPKCS1
+					break;
+				case rsa.RsaPSS.ALGORITHM_NAME.toLowerCase():
+					algClass = rsa.RsaPSS
+					break;
+				default:
+					throw new TypeError("Unsupported algorith in use");
+			}
+			var signature = algClass.sign(that.session, _alg, key, data);
 			resolve(signature);
+		})
+	}
+	
+	verify(algorithm: iwc.AlgorithmType, key: CryptoKey, signature: Buffer, data: Buffer): Promise {
+		var that = this;
+		return new Promise(function(resolve, reject) {
+			var _alg: iwc.IAlgorithmIdentifier = { name: "" };
+			if (algorithm instanceof String) {
+				_alg = { name: algorithm };
+			}
+			else {
+				_alg = <iwc.IAlgorithmIdentifier>algorithm;
+			}
+			var algClass: alg.IAlgorithmBase = null;
+			switch (_alg.name.toLowerCase()) {
+				case rsa.RsaPKCS1.ALGORITHM_NAME.toLowerCase():
+					algClass = rsa.RsaPKCS1
+					break;
+				case rsa.RsaPSS.ALGORITHM_NAME.toLowerCase():
+					algClass = rsa.RsaPSS
+					break;
+				default:
+					throw new TypeError("Unsupported algorith in use");
+			}
+			var valid = algClass.verify(that.session, _alg, key, signature, data);
+			resolve(valid);
 		})
 	}
 
