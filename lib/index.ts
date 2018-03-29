dd01ac40c04ea4eca2b431dab6e6a8fa7545cff1
Stepan Miroshin <microshine@mail.ru>
2018-03-29 15:52:34 +0000

    Export Storage classes

--- lib/index.ts
+++ lib/index.ts
@@ -3,4 +3,7 @@ require("babel-polyfill");
 export { WebCrypto } from "./webcrypto";
 export { SubtleCrypto } from "./subtle";
 export { CryptoKey } from "./key";
+export { KeyStorage } from "./key_storage";
+export { CertificateStorage } from "./cert_storage";
+export { CryptoCertificate, X509Certificate, X509CertificateRequest } from "./cert";
 export * from "./provider";
